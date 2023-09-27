eb7e76d90e1afb48d8ee7fc17bf082f0854615d8
EvgeniaBabulkova <113165892+EvgeniaBabulkova@users.noreply.github.com>
2023-09-27 09:40:26 +0200

    1st version done

--- script.js
+++ script.js
@@ -1,33 +1,53 @@
-$(document).ready(function () {
-    // Get all the navigation links and sections
-    var $navLinks = $('.nav-link');
-    var $highlightableSections = $('.highlightable');
 
-    // Function to check if a section is in view
-    function isSectionInView(section) {
-        var windowTop = $(window).scrollTop();
-        var windowBottom = windowTop + $(window).height();
-        var sectionTop = section.offset().top;
-        var sectionBottom = sectionTop + section.height();
+// FOR SMOOTH SCROLLING TO WORK------------------
 
-        return windowTop >= sectionTop && windowBottom <= sectionBottom;
+document.addEventListener('DOMContentLoaded', function () {
+  // Select the "WORK" link
+  const workLink = document.querySelector('.work-link');
+
+  // Define the height of your header (adjust this value as needed)
+  const headerHeight = 100; // Change this to match your header's height
+
+  // Add a click event listener to the link
+  workLink.addEventListener('click', function (e) {
+    // Prevent the default behavior of the link
+    e.preventDefault();
+
+    // Find the first section with the "work-section" class
+    const targetSection = document.querySelector('.work-section');
+
+    if (targetSection) {
+      // Calculate the scroll position, accounting for the header height
+      const targetPosition = targetSection.getBoundingClientRect().top + window.scrollY - headerHeight;
+
+      // Scroll to the target position smoothly
+      window.scrollTo({
+        top: targetPosition,
+        behavior: 'smooth'
+      });
     }
-
-    // Function to update the highlight based on the current section in view
-    function updateHighlight() {
-        $highlightableSections.each(function () {
-            var $section = $(this);
-            var sectionId = $section.attr('id');
-            var $navLink = $navLinks.filter('[href="#' + sectionId + '"]');
-
-            if (isSectionInView($section)) {
-                $navLink.addClass('highlighted');
-            } else {
-                $navLink.removeClass('highlighted');
-            }
-        });
-    }
-
-    // Call the updateHighlight function on page load and scroll
-    $(window).on('load scroll', updateHighlight);
+  });
 });
+
+
+
+
+
+// FOR MAKING MY BUTTON CLICKABLE WITHOUT SCREWING UP MY STYLING
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Select the button element by its id
+  const aboutMeButton = document.getElementById('aboutMeButton');
+
+  // Add a click event listener to the button
+  aboutMeButton.addEventListener('click', function () {
+    // Redirect to the "About Me" page when the button is clicked
+    window.location.href = 'aboutme.html';
+  });
+});
+
+
+
+
+
+
