1a86712e35bc5353dad2a7a3845a6cebe8f91e7c
EvgeniaBabulkova <113165892+EvgeniaBabulkova@users.noreply.github.com>
2023-10-06 21:12:53 +0200

    fixing links and buttons

--- header.js
+++
@@ -1,9 +0,0 @@
-// function loadHeader() {
-//     // Fetch the header.html content
-//     fetch('header.html')
-//         .then(response => response.text())
-//         .then(data => {
-//             // Insert the header HTML into a designated container
-//             document.getElementById('header-container').innerHTML = data;
-//         });
-// }
